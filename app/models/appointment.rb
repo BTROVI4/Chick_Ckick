4c0b5184a1008ca53651b80d1c353ecb0606f82b
BTROVI4 <boghdan.btr@gmail.com>
2020-08-11 16:34:46 +0300

    API

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,36 +1,35 @@
 class Appointment < ApplicationRecord
   belongs_to :specialist, foreign_key: "specialist_id", class_name: "Specialist"
-  # belongs_to :service, optional: true
 
-  # before_validation :set_end_date
-  # validate :appointment_check
+  before_validation :set_end_date
+  validate :appointment_check
 
-  # private
+  private
 
-  #   def set_end_date
-  #     # Appointment.joins(:services).where()
-  #     self.end_date = self.start_date + self.service.wminutes.minutes
-  #   end
+    def set_end_date
+      # Appointment.joins(:services).where()
+      self.end_date = self.start_date + self.specialist.service.wminutes.minutes
+    end
 
-  #   def appointment_check
-  #     # valid = self.specialist.appointments.where("(appointments.start_date <= ? AND appointments.end_date >= ?) OR (appointments.start_date <= ? AND appointments.end_date >= ?)", start_date, start_date, end_date, end_date).count == 0
-  #     # self.errors.add(:start_date, "Alreadedy booked")
+    def appointment_check
+      # valid = self.specialist.appointments.where("(appointments.start_date <= ? AND appointments.end_date >= ?) OR (appointments.start_date <= ? AND appointments.end_date >= ?)", start_date, start_date, end_date, end_date).count == 0
+      # self.errors.add(:start_date, "Alreadedy booked")
 
-  #     Appointment.all.each do |appointment|
+      Appointment.all.each do |appointment|
         
-  #       if start_date.to_date == appointment.start_date.to_date
+        if start_date.to_date == appointment.start_date.to_date
 
-  #         # Appointment.where("start_date <= ? AND end_date >= ?", start_date, end_date)
+          # Appointment.where("start_date <= ? AND end_date >= ?", start_date, end_date)
 
-  #         if start_date.to_time < appointment.start_date.to_time && end_date.to_time <= appointment.start_date.to_time || start_date.to_time > appointment.end_date.to_time && end_date.to_time >= appointment.end_date.to_time
+          if start_date.to_time < appointment.start_date.to_time && end_date.to_time <= appointment.start_date.to_time || start_date.to_time > appointment.end_date.to_time && end_date.to_time >= appointment.end_date.to_time
         
-  #         else
-  #           errors.add(:start_date, "Alreadedy booked")
-  #           break     
-  #         end
-  #       end
+          else
+            errors.add(:start_date, "Alreadedy booked")
+            break     
+          end
+        end
 
-  #     end
+      end
 
-  #   end
+    end
 end
