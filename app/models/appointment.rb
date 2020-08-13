79c132c3f6da9081bf402ae18c3fa55c097f4de5
BTROVI4 <boghdan.btr@gmail.com>
2020-08-13 17:02:35 +0300

    Fix

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -7,20 +7,23 @@ class Appointment < ApplicationRecord
   private
 
     def set_end_date
+      # Appointment.joins(:services).where()
       self.end_date = self.start_date + self.specialist.service.wminutes.minutes
     end
 
     def appointment_check
       # valid = self.specialist.appointments.where("(appointments.start_date <= ? AND appointments.end_date >= ?) OR (appointments.start_date <= ? AND appointments.end_date >= ?)", start_date, start_date, end_date, end_date).count == 0
       # self.errors.add(:start_date, "Alreadedy booked")
-
-      self.Appointment.all.each do |appointment|
+ 
+      
+      self.specialist.appointments.each do |appointment|
         
         if start_date.to_date == appointment.start_date.to_date
 
           # Appointment.where("start_date <= ? AND end_date >= ?", start_date, end_date)
 
-          if start_date.to_time < appointment.start_date.to_time && end_date.to_time <= appointment.start_date.to_time || start_date.to_time > appointment.end_date.to_time && end_date.to_time >= appointment.end_date.to_time        
+          if start_date.to_time < appointment.start_date.to_time && end_date.to_time <= appointment.start_date.to_time || start_date.to_time > appointment.end_date.to_time && end_date.to_time >= appointment.end_date.to_time
+        
           else
             errors.add(:start_date, "Alreadedy booked")
             break     
